ed549e8e1e505be8460829051247078fc3597459
Ezydenias <kleinermaximilian@googlemail.com>
2018-11-15 18:37:35 +0100

    Completed Server and Client class

--- src/FECpacket.java
+++ src/FECpacket.java
@@ -2,40 +2,54 @@ import java.util.Arrays;
 
 public class FECpacket {
 
-  int FEC_group;       // FEC-Gruppengröße
+    int FEC_group;       // FEC-Gruppengröße
 
-  byte[][] mediastack; // Puffer für Medienpakete
-  byte[][] fecstack;   // Puffer für FEC-Pakete
+    byte[][] mediastack; // Puffer für Medienpakete
+    byte[][] fecstack;   // Puffer für FEC-Pakete
 
-  // SENDER --------------------------------------
-  public FECpacket()
+    // SENDER --------------------------------------
+    public FECpacket() {
+        this(0);
+    }
 
-  // RECEIVER ------------------------------------
-  public FECpacket(int FEC_group)
+    // RECEIVER ------------------------------------
+    public FECpacket(int FEC_group) {
+        this.FEC_group = FEC_group;
+    }
 
-  // ----------------------------------------------
-  // *** SENDER ***
-  // ----------------------------------------------
+    // ----------------------------------------------
+    // *** SENDER ***
+    // ----------------------------------------------
 
-  // speichert Nutzdaten zur FEC-Berechnung
-  public void setdata(byte[] data, int data_length)
+    // speichert Nutzdaten zur FEC-Berechnung
+    public void setdata(byte[] data, int data_length) {
+    }
 
-  // holt fertiges FEC-Paket, Rückgabe: Paketlänge
-  public int getdata(byte[] data)
+    // holt fertiges FEC-Paket, Rückgabe: Paketlänge
+    public int getdata(byte[] data) {
+        return 0;
+    }
 
 
-  // ------------------------------------------------
-  // *** RECEIVER ***
-  // ------------------------------------------------
-  // speichert UDP-Payload, Nr. des Bildes
-  public void rcvdata(int nr, byte[] data)
+    // ------------------------------------------------
+    // *** RECEIVER ***
+    // ------------------------------------------------
+    // speichert UDP-Payload, Nr. des Bildes
+    public void rcvdata(int nr, byte[] data) {
+    }
 
-  // speichert FEC-Daten, Nr. eines Bildes der Gruppe
-  public void rcvfec(int nr, byte[] data)
+    // speichert FEC-Daten, Nr. eines Bildes der Gruppe
+    public void rcvfec(int nr, byte[] data) {
+    }
 
-  // übergibt vorhandenes/korrigiertes Paket oder Fehler (null)
-  public byte[] getjpeg(int nr)
+    // übergibt vorhandenes/korrigiertes Paket oder Fehler (null)
+    public byte[] getjpeg(int nr) {
+        byte[] result = new byte[0];
+        return result;
+    }
 
-  // für Statistik, Anzahl der korrigierten Pakete
-  public int getNrCorrected()
+    // für Statistik, Anzahl der korrigierten Pakete
+    public int getNrCorrected() {
+        return 0;
+    }
 }
